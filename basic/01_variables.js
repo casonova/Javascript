c16609dc1f10004251630d1f3adf71c07b0808c5
Muhammad Rehan Habib <rehan.habib777@gmail.com>
2024-05-11 13:25:42 +0000

    basics

---
+++ basic/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let username = "Test@gmail.com"
+var password = "1wer234" 
+console.log(accountId)
+console.table([accountId,username,password])
+
+/* perfer not to use var because 
+of functional and block scope */
---
+++ basic/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // for newer version of javascript
+
+// alert(3+3) we are using node not browser therfore this syntax of alert is not working
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
